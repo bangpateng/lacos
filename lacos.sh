06af7c51a8bea4f13a5e538c6c73b95584f83792
Bang Pateng <38981255+bangpateng@users.noreply.github.com>
2024-08-15 12:34:21 +0700

    Update lacos.sh

--- lacos.sh
+++ lacos.sh
@@ -3,22 +3,19 @@
 # Mengambil logo dari URL
 source <(curl -s https://raw.githubusercontent.com/bangpateng/symphony/main/logo.sh)
 
-# Meminta input dari pengguna
 read -p "Enter Moniker (nama Validator): " MONIKER
 read -p "Enter Wallet (isi tulisan wallet): " KEY
 read -p "Enter Mnemonic Phrase (frasa mnemonic 12 kata): " MNEMONIC_PHRASE
 
-# Default port yang akan digunakan
-laconicd_PORT=335
+laconicd_PORT=375
 
-# Menyimpan variabel ke dalam .bash_profile
 echo "export KEY=$KEY" >> $HOME/.bash_profile
 echo "export MONIKER=$MONIKER" >> $HOME/.bash_profile
 echo "export laconicd_CHAIN_ID=laconic_9000-1" >> $HOME/.bash_profile
 echo "export laconicd_PORT=$laconicd_PORT" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 
-# Menampilkan konfigurasi yang telah dimasukkan
+
 echo "------------------------------------------------------------------------------------"
 echo -e "Moniker: \e[1m\e[32m$MONIKER\e[0m"
 echo -e "Wallet: \e[1m\e[32m$KEY\e[0m"
@@ -27,15 +24,13 @@ echo -e "Node Port: \e[1m\e[32m$laconicd_PORT\e[0m"
 echo "------------------------------------------------------------------------------------"
 sleep 1
 
-# Pembaruan paket
 echo "1. Updating packages..." && sleep 1
 sudo apt update
 
-# Instalasi dependensi
+
 echo "2. Installing dependencies..." && sleep 1
 sudo apt install curl git wget htop tmux build-essential jq make lz4 gcc unzip -y
 
-# Instalasi Go
 echo "3. Installing Go..." && sleep 1
 cd $HOME
 VER="1.22.2"
@@ -50,7 +45,7 @@ source $HOME/.bash_profile
 
 echo $(go version) && sleep 1
 
-# Instalasi binary
+
 echo "4. Installing binary..." && sleep 1
 cd $HOME
 rm -rf laconicd
@@ -58,7 +53,8 @@ git clone https://git.vdb.to/cerc-io/laconicd.git
 cd laconicd
 make install
 
-# Variabel default
+
+
 CHAINID=${laconicd_CHAIN_ID:-"laconic_9000-1"}
 KEYRING=${KEYRING:-"test"}
 DENOM=${DENOM:-"alnt"}
@@ -67,7 +63,6 @@ LOGLEVEL=${LOGLEVEL:-"info"}
 
 input_genesis_file=${GENESIS_FILE}
 
-# Membersihkan dan mengonfigurasi jika diperlukan
 if [ "$1" == "clean" ] || [ ! -d "$HOME/.laconicd/data/blockstore.db" ]; then
   command -v jq > /dev/null 2>&1 || {
     echo >&2 "jq not installed. More info: https://stedolan.github.io/jq/download/"
@@ -95,8 +90,6 @@ if [ "$1" == "clean" ] || [ ! -d "$HOME/.laconicd/data/blockstore.db" ]; then
       mv $HOME/.laconicd/config/tmp_genesis.json $HOME/.laconicd/config/genesis.json
   }
 
- 
-  # Mengupdate genesis.json jika diperlukan
   if [[ "$TEST_REGISTRY_EXPIRY" == "true" ]]; then
     update_genesis '.app_state["registry"]["params"]["record_rent_duration"]="60s"'
     update_genesis '.app_state["registry"]["params"]["authority_grace_period"]="60s"'
@@ -126,7 +119,6 @@ if [ "$1" == "clean" ] || [ ! -d "$HOME/.laconicd/data/blockstore.db" ]; then
   update_genesis '.consensus["params"]["block"]["time_iota_ms"]="1000"'
   update_genesis '.consensus["params"]["block"]["max_gas"]="10000000"'
 
-  # Menyesuaikan konfigurasi untuk MacOS atau Linux
   if [[ "$OSTYPE" == "darwin"* ]]; then
     sed -i '' 's/create_empty_blocks = true/create_empty_blocks = false/g' $HOME/.laconicd/config/config.toml
   else
@@ -145,7 +137,6 @@ if [ "$1" == "clean" ] || [ ! -d "$HOME/.laconicd/data/blockstore.db" ]; then
     sed -i 's/prometheus = false/prometheus = true/g' $HOME/.laconicd/config/config.toml
   fi
 
-  # Menambahkan akun genesis dan gentx
   laconicd genesis add-genesis-account $KEY 1000000000000000000000000000000$DENOM --keyring-backend $KEYRING
   laconicd genesis gentx $KEY $STAKING_AMOUNT$DENOM --keyring-backend $KEYRING --chain-id $CHAINID
   laconicd genesis collect-gentxs
@@ -154,38 +145,6 @@ else
   echo "Using existing database at $HOME/.laconicd. To replace, run '`basename $0` clean'"
 fi
 
-# Fungsi untuk memeriksa dan memperbarui port jika diperlukan
-check_and_update_port() {
-    local default_port=$1
-    local config_file=$2
-    local new_port=$(( default_port + 1 )) # Mengubah port default
-
-    if ss -tulpen | awk '{print $5}' | grep -q ":$default_port$" ; then
-        echo -e "\e[31mPort $default_port already in use.\e[39m"
-        sleep 2
-        if [ -f "$config_file" ]; then
-            sed -i -e "s|:$default_port|:$new_port|" "$config_file"
-            echo -e "\n\e[42mPort $default_port changed to $new_port.\e[0m\n"
-        else
-            echo -e "\e[31mFile $config_file not found.\e[39m"
-        fi
-        sleep 2
-    fi
-}
-
-# Memeriksa dan memperbarui port
-echo "5. Checking and updating ports..." && sleep 1
-check_and_update_port 26656 "/root/.laconicd/config/config.toml"
-check_and_update_port 26657 "/root/.laconicd/config/config.toml"
-check_and_update_port 26658 "/root/.laconicd/config/config.toml"
-check_and_update_port 6060 "/root/.laconicd/config/config.toml"
-check_and_update_port 1317 "/root/.laconicd/config/app.toml"
-check_and_update_port 9090 "/root/.laconicd/config/app.toml"
-check_and_update_port 9091 "/root/.laconicd/config/app.toml"
-check_and_update_port 8545 "/root/.laconicd/config/app.toml"
-check_and_update_port 8546 "/root/.laconicd/config/app.toml"
-
-# Membuat file service systemd
 SERVICE_FILE=/etc/systemd/system/laconicd.service
 
 cat <<EOF | sudo tee $SERVICE_FILE
@@ -203,6 +162,79 @@ LimitNOFILE=4096
 [Install]
 WantedBy=multi-user.target
 EOF
+
+DAEMON_NAME="laconicd"
+DAEMON_HOME="$HOME/.laconicd"
+echo -e '\n\e[42mChecking a ports\e[0m\n' && sleep 1
+
+DAEMON_HOME="$HOME/.laconicd"
+PORT=335
+if ss -tulpen | awk '{print $5}' | grep -q ":26656$" ; then
+    echo -e "\e[31mPort 26656 already in use.\e[39m"
+    sleep 2
+    sed -i -e "s|:26656\"|:${PORT}56\"|g" $DAEMON_HOME/config/config.toml
+    echo -e "\n\e[42mPort 26656 changed to ${PORT}56.\e[0m\n"
+    sleep 2
+fi
+if ss -tulpen | awk '{print $5}' | grep -q ":26657$" ; then
+    echo -e "\e[31mPort 26657 already in use\e[39m"
+    sleep 2
+    sed -i -e "s|:26657\"|:${PORT}57\"|" $DAEMON_HOME/config/config.toml
+    echo -e "\n\e[42mPort 26657 changed to ${PORT}57.\e[0m\n"
+    sleep 2
+    $DAEMON_NAME config node tcp://localhost:${PORT}57
+fi
+if ss -tulpen | awk '{print $5}' | grep -q ":26658$" ; then
+    echo -e "\e[31mPort 26658 already in use.\e[39m"
+    sleep 2
+    sed -i -e "s|:26658\"|:${PORT}58\"|" $DAEMON_HOME/config/config.toml
+    echo -e "\n\e[42mPort 26658 changed to ${PORT}58.\e[0m\n"
+    sleep 2
+fi
+if ss -tulpen | awk '{print $5}' | grep -q ":6060$" ; then
+    echo -e "\e[31mPort 6060 already in use.\e[39m"
+    sleep 2
+    sed -i -e "s|:6060\"|:${PORT}60\"|" $DAEMON_HOME/config/config.toml
+    echo -e "\n\e[42mPort 6060 changed to ${PORT}60.\e[0m\n"
+    sleep 2
+fi
+if ss -tulpen | awk '{print $5}' | grep -q ":1317$" ; then
+    echo -e "\e[31mPort 1317 already in use.\e[39m"
+    sleep 2
+    sed -i -e "s|:1317\"|:${PORT}17\"|" $DAEMON_HOME/config/app.toml
+    echo -e "\n\e[42mPort 1317 changed to ${PORT}17.\e[0m\n"
+    sleep 2
+fi
+if ss -tulpen | awk '{print $5}' | grep -q ":9090$" ; then
+    echo -e "\e[31mPort 9090 already in use.\e[39m"
+    sleep 2
+    sed -i -e "s|:9090\"|:${PORT}90\"|" $DAEMON_HOME/config/app.toml
+    echo -e "\n\e[42mPort 9090 changed to ${PORT}90.\e[0m\n"
+    sleep 2
+fi
+if ss -tulpen | awk '{print $5}' | grep -q ":9091$" ; then
+    echo -e "\e[31mPort 9091 already in use.\e[39m"
+    sleep 2
+    sed -i -e "s|:9091\"|:${PORT}91\"|" $DAEMON_HOME/config/app.toml
+    echo -e "\n\e[42mPort 9091 changed to ${PORT}91.\e[0m\n"
+    sleep 2
+fi
+if ss -tulpen | awk '{print $5}' | grep -q ":8545$" ; then
+    echo -e "\e[31mPort 8545 already in use.\e[39m"
+    sleep 2
+    sed -i -e "s|:8545\"|:${PORT}45\"|" $DAEMON_HOME/config/app.toml
+    echo -e "\n\e[42mPort 8545 changed to ${PORT}45.\e[0m\n"
+    sleep 2
+fi
+if ss -tulpen | awk '{print $5}' | grep -q ":8546$" ; then
+    echo -e "\e[31mPort 8546 already in use.\e[39m"
+    sleep 2
+    sed -i -e "s|:8546\"|:${PORT}46\"|" $DAEMON_HOME/config/app.toml
+    echo -e "\n\e[42mPort 8546 changed to ${PORT}46.\e[0m\n"
+    sleep 2
+fi
+
+echo "Port checks and updates complete."
 
 sudo systemctl daemon-reload
 sudo systemctl enable laconicd
